80e9d5195b4cfce45ae15e22ca8fa0c1aecaab1c
beimnet777 <91521577+beimnet777@users.noreply.github.com>
2022-03-21 04:38:33 -0700

    Get method implementation view is added

--- Rest/api_basic/serializer.py
+++ Rest/api_basic/serializer.py
@@ -1,6 +1,7 @@
+from pyexpat import model
 from rest_framework import serializers
 from .models import Article
-class ArticleSerializer(serializers.Serializer):
-  title=serializers.CharField(max_length=100)
-  name=serializers.CharField(max_length=100)
-  author=serializers.CharField(max_length=100)
+class ArticleSerializer(serializers.ModelSerializer):
+  class Meta:
+    model=Article
+    fields='__all__'
--- Rest/api_basic/views.py
+++ Rest/api_basic/views.py
@@ -1,3 +1,16 @@
+import imp
+from telnetlib import STATUS
 from django.shortcuts import render
+from django.http import HttpResponse ,JsonResponse
+from .models import Article
+from .serializer import ArticleSerializer
+from rest_framework.parsers import JSONParser
+
 
 # Create your views here.
+def articles_list(request):
+  if request.method=='POST':
+    pass
+  articles=Article.objects.all()
+  serializer=ArticleSerializer(articles,many=True)
+  return JsonResponse(serializer.data,status=200,safe=False)
