e8d2523d802435fa9dc73abf171937e751858e63
beimnet777 <91521577+beimnet777@users.noreply.github.com>
2022-03-21 05:03:42 -0700

    a post method to create article is added

--- Rest/api_basic/views.py
+++ Rest/api_basic/views.py
@@ -1,3 +1,4 @@
+from http import server
 import imp
 from telnetlib import STATUS
 from django.shortcuts import render
@@ -5,12 +6,20 @@ from django.http import HttpResponse ,JsonResponse
 from .models import Article
 from .serializer import ArticleSerializer
 from rest_framework.parsers import JSONParser
+from django.views.decorators.csrf import csrf_exempt
 
 
 # Create your views here.
+@csrf_exempt
 def articles_list(request):
   if request.method=='POST':
-    pass
+    data =JSONParser().parse(request)
+    serialized=ArticleSerializer(data=data)
+    if serialized.is_valid():
+      serialized.save()
+      return JsonResponse(serialized.data,status=201)
+    else:
+      return JsonResponse(serialized.errors,status=400)
   articles=Article.objects.all()
   serializer=ArticleSerializer(articles,many=True)
   return JsonResponse(serializer.data,status=200,safe=False)
